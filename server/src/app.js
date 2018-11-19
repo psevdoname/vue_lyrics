e24e61c0c7e3df43844e8c2441162a08f3fe5359
Artem Dovzhenko <temadovzh@gmail.com>
2018-11-19 11:59:15 -0500

    started adding the database

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+require('./db.js')
 
 const app = express()
 
