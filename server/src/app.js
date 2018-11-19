b899f0a1d8c1eb0115301ed0c927c20ab0255826
Artem Dovzhenko <temadovzh@gmail.com>
2018-11-18 21:15:36 -0500

    setting up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 // whats cors for
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hellow orld'
+    message: `user ${req.body.email} ${req.body.password} registered`
   })
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 8081)
