296c770d8a5561ae4c5e7ad2692de70fd02b6208
Artem Dovzhenko <temadovzh@gmail.com>
2018-11-19 20:00:00 -0500

    errors on register fixed, db adds new users, validation not done yet

--- server/src/app.js
+++ server/src/app.js
@@ -6,16 +6,15 @@ const morgan = require('morgan')
 require('./models/db')
 // const session = require('express-session')
 const app = express()
-require('./routes')(app)
+app.use(cors())
 
 app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
 
 // const user = mongoose.model('user')
 // const lyrics = mongoose.model('lyrics')
 
 // whats cors for
-
+require('./routes')(app)
 app.listen(process.env.PORT || 8081)
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -3,47 +3,63 @@ const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
 module.exports = {
-
   register (req, res) {
-    const myData = new User({
+    const newUser = new User({
       email: req.body.email,
-      username: res.body.username,
-      password: res.body.password
+      username: req.body.username,
+      password: req.body.password
     })
-    console.log(myData)
-    // console.log(mongoose.model('User').schema)
-    try {
-      // User()
-      res.send({
-        message: 'hellowow'
-      })
-
-      User.find((err, data) => {
-        if (!err) {
-          console.log(data)
-        }
-      })
-
-      // res.send({
-      //   message: 'req.body'
-      // })
-      // console.log(newUser)
-      // newUser.save((err) => {
-      //   if (!err) {
-      //     console.log('success')
-      //   } else {
-      //     console.log(err)
-      //   }
-      // })
-      // User.find((err, data) => {
-      //   if (!err) {
-      //     console.log(data)
-      //   }
-      // })
-    } catch (err) {
-      res.send({
-        message: err
-      })
-    }
+    newUser.save((err) => {
+      if (err) {
+        res.status(400).send({
+          error: 'email in use'
+        })
+      }
+    })
+    User.find((err, data) => {
+      if (!err) {
+        res.send({
+          message: `${data}`
+        })
+        console.log(newUser.email)
+      }
+    })
   }
+  // async register (req, res) {
+
+  //   console.log(myData)
+  //   try {
+  //     // User()
+  //     res.send({
+  //       message: 'hellowow'
+  //     })
+
+  //     User.find((err, data) => {
+  //       if (!err) {
+  //         console.log(data)
+  //       }
+  //     })
+
+  //     // res.send({
+  //     //   message: 'req.body'
+  //     // })
+  //     // console.log(newUser)
+  //     // newUser.save((err) => {
+  //     //   if (!err) {
+  //     //     console.log('success')
+  //     //   } else {
+  //     //     console.log(err)
+  //     //   }
+  //     // })
+  //     // User.find((err, data) => {
+  //     //   if (!err) {
+  //     //     console.log(data)
+  //     //   }
+  //     // })
+  //   } catch (err) {
+  //     res.send({
+  //       message: err
+  //     })
+  //   }
+  // }
 }
