6d589c89b398b705a0cdd000679ecaa076bfdde6
Artem Dovzhenko <temadovzh@gmail.com>
2018-11-20 04:25:51 -0500

    add song to database

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-// let dbconf
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-// // if we're in PRODUCTION mode, then read the configration from a file
-// // use blocking file io to do this...
-//   const fs = require('fs')
-//   const path = require('path')
-//   const fn = path.join(__dirname, 'config.json')
-//   const data = fs.readFileSync(fn)
+let dbconf
+if (process.env.NODE_ENV === 'PRODUCTION') {
+// if we're in PRODUCTION mode, then read the configration from a file
+// use blocking file io to do this...
+  const fs = require('fs')
+  const path = require('path')
+  const fn = path.join(__dirname, 'config.json')
+  const data = fs.readFileSync(fn)
 
-//   // our configuration file will be in json, so parse it and set the
-//   // conenction string appropriately!
-//   const conf = JSON.parse(data)
-//   dbconf = conf.dbconf
-// } else {
-// // if we're not in PRODUCTION mode, then use
-//   dbconf = 'mongodb://localhost/lyricgenNew'
-// }
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data)
+  dbconf = conf.dbconf
+} else {
+// if we're not in PRODUCTION mode, then use
+  dbconf = 'mongodb://localhost/lyricgenNew'
+}
 
-// mongoose.connect(dbconf, {
-//   useNewUrlParser: true
-// })
-mongoose.connect('mongodb://localhost/lyricgenNew')
+mongoose.connect(dbconf, {
+  useNewUrlParser: true
+})
+// mongoose.connect('mongodb://localhost/lyricgenNew')
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', function (callback) {
